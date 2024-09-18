08b789581326fcd6bfd07b5e079f4a0d668eb973
Taposh321 <taposhbarman422@gmail.com>
2024-09-18 15:40:53 +0600

    ui

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ console.log(distPath)
 const server = createServer(app)
 const io = new Server(server,{
     cors:{
-    origin:"http://localhost:5173"
+    origin:"https://hangman-o3z4.onrender.com:5173"
     }
 });
 
